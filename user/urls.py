73cce9d6e2055b19abf8152ec2b8d493538f2c21
Manu <jihy1325@gmail.com>
2023-01-26 16:52:30 +0900

    register&login html 완료

--- user/urls.py
+++ user/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('rest_auth/', include('dj_rest_auth.urls')),
     path('rest_auth/register/', include('dj_rest_auth.registration.urls')),
     path('allauth/', include('allauth.urls')),
-    path("register/", TemplateView.as_view(template_name="register.html"), name="register")
+    path("register/", TemplateView.as_view(template_name="user/register.html"), name="register"),
+    path('login/', TemplateView.as_view(template_name="user/login.html"), name="login")
 
 ]
