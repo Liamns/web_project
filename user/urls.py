660439bd37203370cac6a0e256a60b79ea3bd0a9
joohyosung <116233098+joohyosung@users.noreply.github.com>
2023-01-30 13:24:55 +0900

    update_2023-01-30

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 from django.views.generic import TemplateView
 from django.urls import path, include
-from apis.views import LoginApi, LogoutApi, RegisterView
+from apis.views import LoginApi, LogoutApi, RegisterView, Activate
 
 
 urlpatterns = [
@@ -10,9 +10,10 @@ urlpatterns = [
     path('rest_auth/register/', include('dj_rest_auth.registration.urls')),
     path('allauth/', include('allauth.urls')),
     path("register/", TemplateView.as_view(template_name="user/register.html"), name="register"),
+    path('/activate/<str:uidb64>/<str:token>', Activate.as_view()),
     path('login/', TemplateView.as_view(template_name="user/login.html"), name="login"),
     path('jwt/login/', LoginApi.as_view(),name='jwt_login'),
     path('jwt/logout/', LogoutApi.as_view(),name='jwt_logout'),
-    path('jwt/register/', RegisterView.as_view(), name='jwt_register'),
+    path('jwt/register/', RegisterView.as_view(),name='jwt_register'),
 ]
 
