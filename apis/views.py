67c5f404ceb1f6546f328aafff50edfa69dbcbd2
Liamns <won2451@gmail.com>
2023-02-01 15:16:06 +0900

    mid

--- apis/views.py
+++ apis/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render, redirect
 from user.models import User, Profile
 from user.serializers import UserSerializer, ProfileSerializer
-from rest_framework.decorators import APIView
+from rest_framework import generics
 from rest_framework.response import Response
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework import generics, viewsets
-import random, string
 import jwt,datetime
-from django.http import Http404
 from django.db.models import Q
+from django.http import Http404
 
 
 from rest_framework import status
@@ -19,150 +16,18 @@ from rest_framework.decorators import APIView, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
-#ys
-from rest_framework.viewsets import ModelViewSet
-from django.db.models import Q, Count, Subquery, OuterRef
-import re
-
-
 #수정사항
 from rest_framework.permissions import AllowAny
-from rest_framework.decorators import APIView, permission_classes
-
 from django.views.decorators.debug import sensitive_post_parameters
 
 from allauth.account.views import SignupView
 from user.forms import UserSignupForm
 
-# 프로필 View
-class UserProfileView(ModelViewSet):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-    # permission_classes = (IsAuthenticatedCustom, )
-
-    def get_queryset(self): 
-        if self.request.method.lower() != "get":
-            return self.queryset
-
-        data = self.request.query_params.dict()
-        data.pop("page", None)
-        keyword = data.pop("keyword", None)
-
-        if keyword:
-            search_fields = (
-                "user__name", "profile_img", "user__email"
-            )
-            query = self.get_query(keyword, search_fields)
-            try:
-                return self.queryset.filter(query).filter(**data).exclude(
-                    Q(user_id=self.request.user.id) |
-                    Q(user__is_superuser=True)
-                ).annotate(
-                    fav_count=Count(self.user_fav_query(self.request.user))
-                ).order_by("-fav_count")
-            except Exception as e:
-                raise Exception(e)
-
-        result = self.queryset.filter(**data).exclude(
-            Q(user_id=self.request.user.id) |
-            Q(user__is_superuser=True)
-        ).annotate(
-            fav_count=Count(self.user_fav_query(self.request.user))
-        ).order_by("-fav_count")
-        return result
-
-    @staticmethod
-    def user_fav_query(user):
-        try:
-            return user.user_favorites.favorite.filter(id=OuterRef("user_id")).values("pk")
-        except Exception:
-            return []
-
-
-    @staticmethod
-
-    def get_query(query_string, search_fields):
-      
-        ''' Returns a query, that is a combination of Q objects. that combination
-        aims to search keywords within a model by testion the give search fields.
-        '''
-      
-      
-        query = None  # Query to search for every search term
-        terms = UserProfileView.normalize_query(query_string)
-        for term in terms:
-            or_query = None  # Query to search for a given term in each field
-            for field_name in search_fields:
-                q = Q(**{"%s__icontains" % field_name: term})
-                if or_query is None:
-                    or_query = q
-                else:
-                    or_query = or_query | q
-            if query is None:
-                query = or_query
-            else:
-                query = query & or_query
-        return query
-
-    @staticmethod
-    def normalize_query(query_string, findterms=re.compile(r'"([^"]+)"|(\S+)').findall, normspace=re.compile(r'\s{2,}').sub):
-      ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
-          and grouping quoted words together
-          
-          Examlple:
-          >>> normalize_query(some random words with quotes and spaces)
-          ['some','random','words','with','quotes','and','spaces']
-      '''
-      return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
 
 #회원가입
 sensitive_post_parameters_m = method_decorator(
     sensitive_post_parameters('password1', 'password2'),
 )
-
-
-@permission_classes([AllowAny])
-class UserRegisterView(RegisterView) :
-
-    serializers = getattr(settings, 'REST_AUTH_REGISTER_SERIALIZERS', {})
-
-    RegisterSerializer = import_callable(serializers.get('REGISTER_SERIALIZER', UserSerializer))
-
-    permission_classes = [AllowAny]
-
-    def get(self, req):
-        user = UserSerializer()
-        return Response({"user" : user}, template_name="user/register.html")
-
-
-
-class ConfirmEmailView(APIView):
-    permission_classes = [AllowAny]
-
-    def get(self, *args, **kwargs):
-        self.object = confirmation = self.get_object()
-        confirmation.confirm(self.request)
-        # A React Router Route will handle the failure scenario
-        return HttpResponseRedirect('/') # 인증성공
-
-    def get_object(self, queryset=None):
-        key = self.kwargs['key']
-        email_confirmation = EmailConfirmationHMAC.from_key(key)
-        if not email_confirmation:
-            if queryset is None:
-                queryset = self.get_queryset()
-            try:
-                email_confirmation = queryset.get(key=key.lower())
-            except EmailConfirmation.DoesNotExist:
-                # A React Router Route will handle the failure scenario
-                return HttpResponseRedirect('/') # 인증실패
-        return email_confirmation
-
-    def get_queryset(self):
-        qs = EmailConfirmation.objects.all_valid()
-        qs = qs.select_related("email_address__user")
-        return qs
-
 
 class UserSignupView(SignupView):
     """
@@ -308,113 +173,29 @@ def jwt_login(response, user):
 
     return response
 
+# profile update
+# class ProfileDetail(APIView):
+    # def get_object(self, pk):
+    #     try:
+    #         return User.objects.get(id=pk)
+    #     except User.DoesNotExist:
+    #         raise Http404
 
-# JWT Social
-from django.conf import settings
-from user.models import User
-from allauth.socialaccount.models import SocialAccount
-from dj_rest_auth.registration.views import SocialLoginView
-from allauth.socialaccount.providers.google import views as google_view
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from django.http import JsonResponse
-import requests
-from rest_framework import status
-from json.decoder import JSONDecodeError
-import os
+    # def get(self, request, pk, format=None):
+    #     user = self.get_object(pk)
+    #     serializer = UserSerializer(user)
+    #     return Response(serializer.data)
 
-state = os.environ.get('STATE')
-BASE_URL = 'http://127.0.0.1:8000/'
-GOOGLE_CALLBACK_URI = BASE_URL + 'user/google/callback/'
+    # def put(self, request, pk, format=None):
+    #     user = self.get_object(pk)
+    #     serializer = UserSerializer(user, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class profileUpdateView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'id'
 
-def google_login(request):
-    scope = "https://www.googleapis.com/auth/userinfo.email"
-    client_id = os.environ.get("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
-    return redirect(f"https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&response_type=code&redirect_uri={GOOGLE_CALLBACK_URI}&scope={scope}")
-
-def google_callback(request):
-    client_id = os.environ.get("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
-    client_secret = os.environ.get("SOCIAL_AUTH_GOOGLE_SECRET")
-    code = request.GET.get('code')
-
-    # 1. 받은 코드로 구글에 access token 요청
-    token_req = requests.post(f"https://oauth2.googleapis.com/token?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type=authorization_code&redirect_uri={GOOGLE_CALLBACK_URI}&state={state}")
-    
-    ### 1-1. json으로 변환 & 에러 부분 파싱
-    token_req_json = token_req.json()
-    error = token_req_json.get("error")
-
-    ### 1-2. 에러 발생 시 종료
-    if error is not None:
-        raise JSONDecodeError(error)
-
-    ### 1-3. 성공 시 access_token 가져오기
-    access_token = token_req_json.get('access_token')
-    id_token = token_req_json.get('id_token')
-    
-
-    #################################################################
-
-    # 2. 가져온 access_token으로 이메일값을 구글에 요청
-    email_req = requests.get(f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}")
-    email_req_status = email_req.status_code
-
-    ### 2-1. 에러 발생 시 400 에러 반환
-    if email_req_status != 200:
-        return JsonResponse({'err_msg': 'failed to get email'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    ### 2-2. 성공 시 이메일 가져오기
-    email_req_json = email_req.json()
-    email = email_req_json.get('email')
-
-    # return JsonResponse({'access': access_token, 'email':email})
-
-    #################################################################
-
-    # 3. 전달받은 이메일, access_token, code를 바탕으로 회원가입/로그인
-    try:
-        # 전달받은 이메일로 등록된 유저가 있는지 탐색
-        user = User.objects.get(email=email)
-
-        # FK로 연결되어 있는 socialaccount 테이블에서 해당 이메일의 유저가 있는지 확인
-        social_user = SocialAccount.objects.get(user=user)
-
-        # 있는데 구글계정이 아니어도 에러
-        if social_user.provider != 'google':
-            return JsonResponse({'err_msg': 'no matching social type'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # 이미 Google로 제대로 가입된 유저 => 로그인 & 해당 우저의 jwt 발급
-        data = {'access_token': access_token, 'code': code, 'id_token':id_token}
-        accept = requests.post(f"{BASE_URL}user/google/login/finish/", data=data)
-        accept_status = accept.status_code
-
-        # 뭔가 중간에 문제가 생기면 에러
-        if accept_status != 200:
-            return JsonResponse({'err_msg': 'failed to signin'}, status=accept_status)
-
-        accept_json = accept.json()
-        accept_json.pop('user', None)
-        return JsonResponse(accept_json)
-
-    except User.DoesNotExist:
-        # 전달받은 이메일로 기존에 가입된 유저가 아예 없으면 => 새로 회원가입 & 해당 유저의 jwt 발급
-        data = {'access_token': access_token, 'code': code, 'id_token':id_token}
-        accept = requests.post(f"{BASE_URL}user/google/login/finish/", data=data)
-        accept_status = accept.status_code
-
-        # 뭔가 중간에 문제가 생기면 에러
-        if accept_status != 200:
-            return JsonResponse({'err_msg': 'failed to signup', "data": data}, status=accept_status)
-
-        accept_json = accept.json()
-        accept_json.pop('user', None)
-        return JsonResponse(accept_json)
-        
-    except SocialAccount.DoesNotExist:
-        # User는 있는데 SocialAccount가 없을 때 (=일반회원으로 가입된 이메일일때)
-        return JsonResponse({'err_msg': 'email exists but not social user'}, status=status.HTTP_400_BAD_REQUEST)
-
-class GoogleLogin(SocialLoginView):
-    adapter_class = google_view.GoogleOAuth2Adapter
-    callback_url = GOOGLE_CALLBACK_URI
-    client_class = OAuth2Client
-# JWT Social
+# profile update
