c36c35f920b1618d889c93d394517f07b31e8ba1
GodDigda2 <116233235+GodDigda2@users.noreply.github.com>
2023-02-01 11:08:50 +0900

    2.1

--- apis/views.py
+++ apis/views.py
@@ -3,7 +3,7 @@ from user.models import User, Profile
 from user.serializers import UserSerializer, ProfileSerializer
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
-
+from django.views.decorators.csrf import csrf_exempt
 
 import jwt,datetime
 
