d277008fd5291658f62f15aeb4cc841b15b4cc8c
joohyosung <116233098+joohyosung@users.noreply.github.com>
2023-02-02 14:18:45 +0900

    update_2023-02-02
    post_main 폼 틀 작업

--- post/urls.py
+++ post/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from . import views
+from .views import *
 from django.urls import reverse_lazy
 
 urlpatterns = [   
-    # http://127.0.0.1:8000/post
-    path("", views.PostView.as_view(), name="post"),
-    path("create/", views.post_create, name="post_create"),
-    path("<int:pk>/", views.detail, name="detail"),
+    # http://127.0.0.1:8000/post/
+    path("", PostView.as_view(), name="post_main"),
+    path("create/", PostCreateView.as_view(), name="post_create"),
+    path("<int:pk>/", detail, name="detail"),
 
     # 이벤트 리스트 
-    path("events/", views.PostEventView.as_view(), name="event_list"),
+    path("events/", PostEventView.as_view(), name="event_list"),
 
     # 이벤트 작성 폼
-    path("events/forms/", views.PostDetailView.as_view(), name="event_detail")
+    # path("events/forms/", PostDetailView.as_view(), name="event_detail")
 ]
