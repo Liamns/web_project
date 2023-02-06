826cce68a35ac3f835bfc9277bbd82c1b8b37d48
Liamns <won2451@gmail.com>
2023-02-06 14:03:00 +0900

    setting

--- config/settings.py
+++ config/settings.py
@@ -184,7 +184,6 @@ MEDIA_URL = '/media/'
 # simple jwt 공식문서
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
