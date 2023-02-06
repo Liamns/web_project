c15e9fc3cbfea0854226bbfc963155b3ea634ebd
tiamomagic <tiamomagic@gmail.com>
2023-02-06 14:43:06 +0900

    save

--- dm/forms.py
+++
@@ -1,9 +0,0 @@
-
-from django import forms
-
-
-class ThreadForm(forms.Form):
-    username = forms.CharField(label='', max_length=100)
-    
-class MessageForm(forms.Form):
-    message = forms.CharField(label='', max_length=1000)
